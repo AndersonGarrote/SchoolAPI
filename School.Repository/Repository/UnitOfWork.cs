15fbf8b8cb97a5ac0484a51da7273ee05b8d0a96
Anderson <anderson.garrote@opus-software.com.br>
2021-02-12 12:22:12 -0300

    Adicionada transaction no Unit ofWork

--- School.Repository/Repository/UnitOfWork.cs
+++ School.Repository/Repository/UnitOfWork.cs
@@ -1,10 +1,12 @@
-﻿using School.Repository.Context;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using School.Repository.Context;
 
 namespace School.Repository.Repository
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly SchoolDbContext _context;
+        private IDbContextTransaction _transaction;
         public ICourseRepository Courses { get; set; }
         public IStudentRepository Students { get; set; }
         public IProfessorRepository Professors { get; set; }
@@ -25,6 +27,21 @@ namespace School.Repository.Repository
         public int Save()
         {
             return _context.SaveChanges();
+        }
+
+        public void BeginTransaction()
+        {
+             _transaction = _context.Database.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            _transaction.Commit();
+        }
+
+        public void Rollback()
+        {
+            _transaction.Rollback();
         }
     }
 }
